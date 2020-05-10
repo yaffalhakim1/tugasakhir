c247dfea2ed204aed0e2455998ba451f1fcb5b4c
Muhammad Yafi Alhakim <yafi.tamfan08@gmail.com>
2020-05-10 10:08:57 +0700

    asistensi

---
+++ cekpajak/src/cekpajak/Cekpajak.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cekpajak;
+
+/**
+ *
+ * @author yafit
+ */
+public class Cekpajak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ cekpajak/src/cekpajak/informasipajak.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cekpajak;
+
+/**
+ *
+ * @author yafit
+ */
+class informasipajak {
+    
+    private String keterangan;
+    //setter
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+    
+    // getter
+    public String getKeterangan(){
+        return keterangan;
+    }
+    
+}
